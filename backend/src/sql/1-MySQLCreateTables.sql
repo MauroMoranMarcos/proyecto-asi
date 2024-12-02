15b2ea54d7e1c45908cf31efdde13c6002d2e83f
Mauro Morán <mauro.moranm@udc.es>
2024-12-02 13:56:29 +0100

    added table User to .sql file

--- backend/src/sql/1-MySQLCreateTables.sql
+++ backend/src/sql/1-MySQLCreateTables.sql
@@ -2,3 +2,17 @@
 -- This file stands for creating the tables in DataBase that are going to be
 -- used in the application.
 -------------------------------------------------------------------------------
+DROP TABLE User;
+
+-- Creating table User to store users.
+CREATE TABLE User (
+                      id BIGINT NOT NULL AUTO_INCREMENT,
+                      userName VARCHAR(60) COLLATE latin1_bin NOT NULL,
+                      password VARCHAR(60) NOT NULL,
+                      email VARCHAR(60) NOT NULL,
+                      role TINYINT NOT NULL,
+
+                      CONSTRAINT UserPK PRIMARY KEY (id),
+                      CONSTRAINT UserNameAndEmailUniqueKey UNIQUE (userName, email)
+
+) ENGINE = InnoDB;
