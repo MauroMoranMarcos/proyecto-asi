b5be38adadd92034093b265b3718947f8c16e363
Mauro Morán <mauro.moranm@udc.es>
2024-12-02 14:03:24 +0100

    added user dao

---
+++ backend/src/main/java/backend/model/entities/UserDao.java
@@ -0,0 +1,6 @@
+package backend.model.entities;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserDao extends PagingAndSortingRepository<User, Long> {
+}
