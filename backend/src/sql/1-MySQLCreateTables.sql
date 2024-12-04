13a28223e62f3accbedda8031a6e3e378c521460
Mauro Morán <mauro.moranm@udc.es>
2024-12-04 01:44:43 +0100

    modified .sql file to add box and warehouse

--- backend/src/sql/1-MySQLCreateTables.sql
+++ backend/src/sql/1-MySQLCreateTables.sql
@@ -3,6 +3,8 @@
 -- used in the application.
 -------------------------------------------------------------------------------
 DROP TABLE User;
+DROP TABLE Box;
+DROP TABLE Warehouse;
 
 -- Creating table User to store users.
 CREATE TABLE User (
@@ -16,3 +18,31 @@ CREATE TABLE User (
                       CONSTRAINT UserNameAndEmailUniqueKey UNIQUE (userName, email)
 
 ) ENGINE = InnoDB;
+
+-- Creating table Warehouse to store info about the Warehouses of the company
+CREATE TABLE Warehouse (
+
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+
+    CONSTRAINT WarehousePK PRIMARY KEY (id)
+
+) ENGINE = InnoDB;
+
+-- Creating table Box to store info about Boxes with its items that are stored in a Warehouse
+CREATE TABLE Box (
+
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    itemName VARCHAR(60) NOT NULL,
+    numItems BIGINT NOT NULL,
+    referenceCode VARCHAR(300) NOT NULL,
+    barCode VARCHAR(300) NOT NULL,
+    manufacturerRef VARCHAR(300) NOT NULL,
+    supplier VARCHAR(120) NOT NULL,
+    warehouseId BIGINT NOT NULL,
+
+    CONSTRAINT BoxPK PRIMARY KEY (id),
+    CONSTRAINT BoxWarehouseIdFK FOREIGN KEY (warehouseId)
+        REFERENCES Warehouse(id)
+
+)
