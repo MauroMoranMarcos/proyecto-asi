b06e989361c96cf751dc2918c5faf3c1a8131433
Manuel Pérez Sueiro <manuel.psueiro@udc.es>
2024-12-28 20:07:19 +0100

    Creada interfaz de orderService con todos los métodos restantes

---
+++ backend/src/main/java/backend/model/services/OrderService.java
@@ -0,0 +1,23 @@
+package backend.model.services;
+
+import backend.model.entities.Order;
+import backend.model.exceptions.InstanceNotFoundException;
+import backend.model.exceptions.PermissionException;
+
+import java.util.List;
+
+public interface OrderService {
+
+    Order createOrder(Long userId, Order order) throws PermissionException;
+
+    Order updateOrder(Long userId, Order order) throws PermissionException, InstanceNotFoundException;
+
+    void deleteOrderById(Long userId, Long orderId) throws PermissionException, InstanceNotFoundException;
+
+    void sendOrderToAdmins(Long userId, Long orderId) throws PermissionException, InstanceNotFoundException;
+
+    void setOrderDone(Long userId, Long orderId) throws PermissionException, InstanceNotFoundException;
+
+    List<Order> findOrderHistory(Long userid);
+    
+}
