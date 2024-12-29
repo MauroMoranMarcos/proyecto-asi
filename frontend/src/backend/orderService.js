085ebeb9698971ea62f2061649b32977a0aa27fe
Manuel Pérez Sueiro <manuel.psueiro@udc.es>
2024-12-29 15:30:24 +0100

    Implementados métodos en frontend para realizar las peticiones para crear y modificar un pedido

---
+++ frontend/src/backend/orderService.js
@@ -0,0 +1,17 @@
+import {config, appFetch} from './appFetch';
+
+export const createOrder = (onSuccess, onErrors) =>
+    appFetch(`/orders/createOrder`,
+        config('POST'), onSuccess, onErrors);
+
+export const addBoxToOrder = (orderId, itemId, numBoxes, numItemsInBox, onSuccess, onErrors) =>
+    appFetch(`/orders/${orderId}/addBox?itemId=${itemId}&numBoxes=${numBoxes}&numItemsInBox=${numItemsInBox}`,
+        config(`POST`), onSuccess, onErrors);
+
+export const updateNumBoxesInOrder = (orderId, boxId, newNumberOfBoxes, onSuccess, onErrors) =>
+    appFetch(`/orders/${orderId}/box/${boxId}?newNumberOfBoxes=${newNumberOfBoxes}`,
+        config(`PUT`), onSuccess, onErrors);
+
+export const deleteBoxInOrder = (orderId, boxId, onSuccess, onErrors) =>
+    appFetch(`/orders/${orderId}/box/${boxId}/delete`,
+        config(`POST`), onSuccess, onErrors);
